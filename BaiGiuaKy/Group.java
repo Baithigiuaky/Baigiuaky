8abcf22bbbb31513e2175f1e608f252006865b55
Group18 <23130056@st.hcmuaf.edu.vn>
2024-12-22 15:12:37 +0700

    Update Group.java

--- BaiGiuaKy/Group.java
+++ BaiGiuaKy/Group.java
@@ -1,5 +1,6 @@
 package BaiGiuaKy;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,6 +10,9 @@ public class Group {
 	private String baiHat;
 	private double diemBinhChon;
 	private List<AnhTai> listOfAnhTaiBiLoai;
+	private HashMap<ArrayList<AnhTai>, BaiHat> baihatnhom;
+	private int diemCuoiCung;
+	
 	/**
 	 * @param tenNhom
 	 * @param listOfThanhVien
@@ -18,8 +22,14 @@ public class Group {
 	 */
 	public Group(String tenNhom, HashMap<AnhTai, BaiHat> baihatCaNhan, String baihat) {
 		this.tenNhom = tenNhom;
-		this.baihatCaNhan = baihatCaNhan;
-		this.baiHat = baihat;
+		this.baihatCaNhan = baihatCaNhan != null ? baihatCaNhan : new HashMap<>();
+		this.baiHat = baihat != null ? baihat : "Chưa có bài hát";
+	}
+	
+	public Group(String tenNhom, HashMap<ArrayList<AnhTai>, BaiHat> baihatnhom, int point) {
+		this.tenNhom = tenNhom;
+		this.baihatnhom = baihatnhom != null ? baihatnhom : new HashMap<>();
+		this.diemCuoiCung = point;
 	}
 	public String getTenNhom() {
 		return tenNhom;
@@ -45,8 +55,58 @@ public class Group {
 		    System.out.println(key + ": " + value);
 		}
 	}
+	
 	@Override
 	public String toString() {
-		return "Tên Nhóm: " + tenNhom + "\n" + "Tác Phẩm: " + "\n" + baihatCaNhan + "\n" + "Tên Bài Hát Chung: " + baiHat;
+		return "Tên Nhóm: " + tenNhom + "\n" + "Tác Phẩm: " + "\n" + baihatCaNhan + "\n" + "Tên Bài Hát Chung: " + baiHat + "\n" + "--------------------------" + "\n";
 	}
+	public HashMap<ArrayList<AnhTai>, BaiHat> getBaihatnhom() {
+		return baihatnhom;
+	}
+	public void setBaihatnhom(HashMap<ArrayList<AnhTai>, BaiHat> baihatnhom) {
+		this.baihatnhom = baihatnhom;
+	}
+
+	public int getDiemCuoiCung() {
+		return diemCuoiCung;
+	}
+
+	public void setDiemCuoiCung(int diemCuoiCung) {
+		this.diemCuoiCung = diemCuoiCung;
+	}
+	 
+    public static void findTwoGroupsWithLowestScores(List<Group> groups) {
+        if (groups.size() < 2) {
+            System.out.println("Không đủ nhóm để tìm.");
+            return;
+        }
+
+        // Khởi tạo 2 nhóm có điểm thấp nhất
+        Group min1 = groups.get(0);
+        Group min2 = groups.get(1);
+
+        // Đảm bảo min1 luôn có điểm thấp hơn min2
+        if (min1.getDiemCuoiCung() > min2.getDiemCuoiCung()) {
+            Group temp = min1;
+            min1 = min2;
+            min2 = temp;
+        }
+
+        // Duyệt qua các nhóm còn lại để tìm ra nhóm có điểm nhỏ nhất và nhóm có điểm nhỏ thứ 2
+        for (int i = 2; i < groups.size(); i++) {
+            Group currentGroup = groups.get(i);
+            if (currentGroup.getDiemCuoiCung() < min1.getDiemCuoiCung()) {
+                min2 = min1;
+                min1 = currentGroup;
+            } else if (currentGroup.getDiemCuoiCung() < min2.getDiemCuoiCung()) {
+                min2 = currentGroup;
+            }
+        }
+        // In kết quả
+        System.out.println("--------------------------------------------");
+        System.out.println("Hai nhóm có điểm nhỏ nhất và lọt vào nhóm nguy hiểm:");
+        System.out.println(min1);
+        System.out.println(min2);
+
+}
 }
